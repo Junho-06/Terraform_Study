6f95834a1a902408a9957903b0909824f045427e
Junho-06 <k34920032@gmail.com>
2023-02-04 00:04:06 +0900

    aws provider -> create test VPC

--- start-tf/main.tf
+++ start-tf/main.tf
@@ -2,6 +2,11 @@ provider "local" {
 
 }
 
+provider "aws" {
+    # 하나의 aws provider는 하나의 리전과 연결됨, 여러 리전이 필요하다면 여러 provider 생성
+    region = "ap-northeast-2"
+}
+
 resource "local_file" "foo" {
     # path.module = 현재 파일이 위치한 경로
     filename = "${path.module}/foo.txt"
@@ -14,4 +19,16 @@ data "local_file" "bar" {
 
 output "file_bar" {
     value = data.local_file.bar
+}
+
+resource "aws_vpc" "foo" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        "Name" = "This is test VPC"
+    }
+}
+
+output "vpc_foo" {
+    value = aws_vpc.foo
 }
