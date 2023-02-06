bdf5e58925ca4db51dd5c17f93949de3bdc80159
Junho-06 <k34920032@gmail.com>
2023-02-06 19:25:57 +0900

    count로 user를 여러명 만들기

--- count,for-each-tf/main.tf
+++ count,for-each-tf/main.tf
@@ -2,8 +2,7 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-# No count / for_each
-
+# No count / No for_each
 resource "aws_iam_user" "user_1" {
   name = "user-1"
 }
@@ -22,4 +21,18 @@ output "user_arns" {
     aws_iam_user.user_2.arn,
     aws_iam_user.user_3.arn,
   ]
+}
+
+# count
+resource "aws_iam_user" "count" {
+  # count는 resource 최상단에 표기 하는것이 컨벤션
+  count = 10
+
+  # count는 index를 사용할 수 있음 (0~9)
+  name = "count-user-${count.index}"
+}
+
+output "count_user_arns" {
+          # count에서 만든 모든 user의 arn
+  value = aws_iam_user.count.*.arn
 }
