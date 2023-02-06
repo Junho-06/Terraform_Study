3400a8b68ea01af5fd134099672dfe7e63bcbea4
Junho-06 <k34920032@gmail.com>
2023-02-06 19:32:41 +0900

    for_each map 방식을 사용하여 유저를 만들기

--- count,for-each-tf/main.tf
+++ count,for-each-tf/main.tf
@@ -36,3 +36,45 @@ output "count_user_arns" {
           # count에서 만든 모든 user의 arn
   value = aws_iam_user.count.*.arn
 }
+
+# for_each
+resource "aws_iam_user" "for_each_set" {
+  for_each = toset([
+    "for-each-set-user-1",
+    "for-each-set-user-2",
+    "for-each-set-user-3",
+  ])
+
+  name = each.key
+}
+
+output "for_each_set_user_arns" {
+  value = values(aws_iam_user.for_each_set).*.arn
+}
+
+resource "aws_iam_user" "for_each_map" {
+  for_each = {
+    alice = {
+        level = "low"
+        manager = "Junho"
+    }
+    bob = {
+        level = "mid"
+        manager = "Junho"
+    }
+    john = {
+        level = "high"
+        manager = "steve"
+    }
+  }
+
+         # alic, bob, john
+  name = each.key
+
+         # 각 키들의 벨류들이 들어감
+  tags = each.value
+}
+
+output "for_each_map_user_arns" {
+  value = values(aws_iam_user.for_each_map).*.arn
+}
